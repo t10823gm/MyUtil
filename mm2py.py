b5560131cf228e95b3ab4d271d26546f123c1094
Gembu <gembu>
2018-12-17 15:16:17 +0900

    all functions are work properly in KTR demo analysis

--- mm2py.py
+++ mm2py.py
@@ -14,8 +14,17 @@ def loadExcelFile(input_file_name):
     print('Number of Sheet: ', num_sheet)
     df = input_book.parse(input_sheet_name[0])
     df.columns = [c.replace(' ', '_') for c in df.columns]
+    df = df[df['Image_Name'] != 'Image Name'] # for multi-postion measurement
     print('Channel Info:', np.unique(np.asarray(df['Image_Name'])))
     return df
+
+def getDataFromDf(df):
+    timepoints = np.array(np.unique(df['Image_Plane']), dtype=np.float32)
+    cell_serial = np.arange(1, int(df.shape[0]/len(timepoints))+1,1) #multi-positionの時は通し番号
+    df['Cell_Serial'] = np.repeat(cell_serial, len(timepoints))
+    #print(int(nuc_df.shape[0]/len(timepoints)))
+    intdata = np.reshape(np.asarray(df['Average_Intensity']), [len(cell_serial), len(timepoints)]).T
+    return timepoints, cell_serial, intdata, df
 
 def channelSpliter(df, channel):
     for ch in np.unique(np.asarray(df['Image_Name'])):
@@ -38,12 +47,69 @@ def normalizeAtStimulus(FC_ratio, timepoint):
         nmFC_ratio[:, i] = nmFC_ratio[:, i]/np.mean(nmFC_ratio[0:timepoint, i])
     return nmFC_ratio
 
-def arrayMeanSd(array, timepoints):
+def arrayMeanSd(array):
+    array = np.array(array, dtype=np.float32)
     array_mean = np.mean(array, axis=1)
+    #array_mean = np.array(np.mean(array, axis=1), dtype=np.float32) 
     array_sd = np.std(array, axis=1)
-    return nm_mean, nm_sd
+    #array_sd = np.array(np.std(array, axis=1), dtype=np.float32)
+    return array_mean, array_sd
 
-def plotShadedErrorBar(array, timepoints)
-    plt.plot(timepoints, array)
-    plt.fill_between(timepoints, array_mean-arrat_sd, array_mean+array_sd, alpha=0.5)
+def plotShadedErrorBar(timepoints, array_mean, array_sd):
+    plt.plot(timepoints, array_mean)
+    print(array_mean-array_sd)
+    print(array_mean+array_sd)
+    plt.fill_between(timepoints, array_mean-array_sd, array_mean+array_sd, alpha=0.5)
     return
+
+def getTargetIndex(df, column_name, keyword):
+    search_array = np.asarray(df[column_name])
+    index = np.where(search_array == keyword)
+    return index
+
+def deleteDataFromArray(array, region_id, del_region_list):
+    mod_region = region_id.copy()
+    #print(mod_region)
+    #print(mod_region.shape)
+    mod_array = array.copy()
+    for i in del_region_list:
+        print(i)
+        idx = np.where(mod_region == i)
+        print(idx)
+        mod_region = np.delete(mod_region,  idx)
+        mod_array = np.delete(mod_array, idx, axis=1)
+    #print(mod_region.shape)
+    return mod_array, mod_region
+
+
+
+
+
+
+
+### Delete in future ###
+def deleteFromDf(df, del_reg, KTR=False):
+    """
+    del_reg : list of region for deletion
+    KTR : KTR analysis
+    """
+    mod_df = df.copy()
+    print(df.shape)
+    for i in del_reg:
+        mod_df = mod_df[mod_df['Region_Label'] != i]
+        if KTR==True:
+            # in KTR analysis, paired region is also delete
+            mod_df = mod_df[mod_df['Region_Label'] != i+1 ]
+    print(df.shape)
+    return mod_df
+
+def extractFromDf(df, ext_reg, KTR=False):
+    """
+    ext_reg : list of region for deletion
+    KTR : KTR analysis
+    """
+    timepoints = np.unique(np.asanyarray(df['Image_Plane']))
+    region_id = np.arange(1, int(df.shape[0]/len(timepoints))+1,1)
+    del_reg = list(set(region_id) - set(ext_reg))
+    mod_df = deleteFromDf(df, del_reg, KTR)
+    return mod_df
