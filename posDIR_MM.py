d5dc0b863d00043bc9a64386127955dffa0473ee
Gembu <gembu>
2019-01-03 16:59:24 +0900

    update posDIR_MM.py

--- posDIR_MM.py
+++ posDIR_MM.py
@@ -4,46 +4,70 @@ Folders were created for each imaging position
 
 Author: Gembu MARYU
 """
-
+from __future__ import print_function
 from os.path import join, relpath, splitext, exists
-from os import makedirs, getcwd, rename
+from os import makedirs, getcwd, rename, listdir
 import shutil
 from glob import glob
 import sys
+import os
+import string
+import argparse
+import json
 
-### Editable variable
-path = "20180331_HELB"
 
-#path=sys.argv[0]
-ROOT_DIR = getcwd()
-#print ROOT_DIR
-#print join(ROOT_DIR, path)
+#python posDIR_MM.py -s /home/gembu/CellTK/data/2014_MCF10A -d /home/gembu/CellTK/data/20181214_MCF10A -t 132'
+parser = argparse.ArgumentParser(
+    prog='posDIR_MM.py',
+    usage='python posDIR_MM.py [-s \'hgoehoge\'] [-d \'fugafuga\'] [-t ]',
+    description='make subfolder for MetaMorph MDA',
+    #add_help=True
+)
 
-files = [relpath(x, path)
+parser.add_argument('-s', '--src', help='full path to the source directory')
+parser.add_argument('-d', '--dest', help='full path to the destination directory')
+parser.add_argument('-t', '--start_tp',  default=0, type=int, help='start timepint')
+arg = parser.parse_args()
 
-for x in glob(join(path, "*"))]
-#print files
+''' params '''
+SRC_DIR = arg.src
+start_tp = arg.start_tp
+DEST_DIR = arg.dest
 
-for file in files:
-    ext = splitext(file)
-    if ext[1] == ".tif" or ext[1] == ".TIF":
-        fn = file.split("_")
-        pi = fn[-2] # position infomation
-        pn = pi[1:]
-        dn = "Pos"+ pn # directoly name
-        ti = fn[-1] # time infomation
-        tn = ti.split('.')[0]
-        tn = tn[1:]
+def cpy_rename(path, DEST_DIR, start_tp = 0):
+    '''
+    Copy TIFF file from external HDD to "CellTK/data" directory after rename
+    :param path: path to 'TimePoint_#' directory
+    :param SRC_DIR: parent directory of each experimental result
+    :param tp: Time point information
+    :return: null
+    '''
+    files = [relpath(x,path)
+             for x in glob(join(path, "*"))]
+    
+    for i, file in enumerate(files):
+        #print(file)
+        ext = splitext(file)
+        if ext[1] == ".tif" or ext[1] == ".TIF":
+            # e.g. : 96well-5col-p21-2-1-HGS-PIP_C07_w1.TIF / 96well-5col_A01_s2_w1.TIF / MCF10A-4_w2GFP-Glass_s1_t84.TIF
+            fn = file.split("_")
+            #print(fn)
+            tp = file.split('_')[-1] # timepoint
+            tp = tp.split('.')[0]
+            tp = tp[1:]
+            w_s = fn[-2] # well info or stage position info
+            posinfo = join(DEST_DIR, fn[-2])
+            #print(posinfo)
+            if exists(join(DEST_DIR, fn[-2])) != True:
+                makedirs(posinfo)
 
-        if exists(join(ROOT_DIR, path, dn)) == True:
-            #print 'Hi'
-            shutil.move(join(ROOT_DIR, path, file), join(ROOT_DIR, path, dn, file))
-        else:
-            print "omg"
-            makedirs(join(path, dn))
-            shutil.move(join(ROOT_DIR, path, file), join(ROOT_DIR, path, dn, file))
+            pn = w_s[1:] # number of stage position
+            afile = fn[1] + '_t' + '{0:03d}'.format(int(tp)+start_tp) + '.TIF'
+            #print(join(posinfo, afile))
+            #print(join(path, file))
+            shutil.copy(join(path, file), join(posinfo, afile))
+        if i % 100 == 0:
+            print(str(i) + "th pic is processed")
+    return
 
-        file1 = join(ROOT_DIR, path, dn, file)
-        file2 = join(ROOT_DIR, path, dn, fn[0]+'_'+fn[1]+'_'+fn[2]+'_'+'t'+'{0:03d}'.format(int(tn))+'.TIF')
-        rename(file1, file2)
-
+cpy_rename(SRC_DIR, DEST_DIR, start_tp)
